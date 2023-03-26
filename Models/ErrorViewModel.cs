8e1083ed70b3c9902236ed396a3707670613e9c9
Notebook <Notebook@DESKTOP-IHF3HIP>
2023-03-26 16:23:14 -0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PROJECT1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
